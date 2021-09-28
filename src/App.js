a829e5b9c38d17922dde1fc558a4bc0940a6da30
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-28 20:13:15 +0300

    Commit 7

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div style={{display: 'flex', justifyContent: 'space-between', margin: 1+'rem', paddingBottom: 1+'rem', borderBottom: 1+'px'+'solid'+'#ccc'}}>
+      <div style={{display: 'flex', justifyContent: 'space-between', margin: 1+'rem', paddingBottom: 1+'rem', borderBottom:'1px solid #ccc'}}>
         <h3 style={{margin: 0+0+0.25+'rem'+0}}>Sushi</h3>
         <div style={{fontStyle: "italic"}}>I prefer steak than sushi.</div>
         <div style={{ marginTop: 0.25+'rem',fontWeight: 'bold',color: '#ad5502',fontSize: 1.25+'rem'}}>$9.99</div>
