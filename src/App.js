9d0d8cd34328263887b22aa4f355e38436ed4d93
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-28 19:32:09 +0300

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      <div className="wrapper">
-        <h3 className="mealHeading">Sushi</h3>
-        <div className="description">I prefer steak than sushi.</div>
-        <div className="price">$9.99</div>
+      <div className="wrapper"  style={{display: 'flex', justifyContent: 'space-between', margin: 1+'rem', paddingBottom: 1+'rem', borderBottom:  1+'px' + 'solid #ccc' }}>
+        <h3 className="mealHeading" style={{margin: 0+0+0.25+'rem'+0}}>Sushi</h3>
+        <div className="description" style={{fontStyle: "italic"}}>I prefer steak than sushi.</div>
+        <div className="price" style={{ marginTop: 0.25+'rem',fontWeight: 'bold',color: '#ad5502',fontSize: 1.25+'rem'}}>$9.99</div>
       </div>
     </div>
   );
